deb170db994d4fb648e45b402c285f49b09ee96e
= <=>
2024-11-05 17:48:41 -0300

    ok

--- aplicativo/aula3.py
+++ aplicativo/aula3.py
@@ -1,7 +1,8 @@
 #TODO: Objetivo
-  # 1. Capturar video usando OpenCV 
-  # 2. Processar o frame usando MediaPipe
-  # 3. Desenhar os pontos  
+  # 1. Entender as coordenadas do MediaPipe 
+  # 2. Converter as coordenadas
+  # 3. Distância Euclidiana (1D,2D,3D, n....) 
+  # 4. Média (cálculo do EAR) 
 
 
 import cv2 #pip install opencv-python
@@ -17,6 +18,12 @@ mp_drawing = mp.solutions.drawing_utils
 # coletar solução do Face Mesh
 mp_face_mesh = mp.solutions.face_mesh
 
+#FIXME:lista dos pontos do olho esquerdo
+p_olho_esq = [385,380,387,373,362,263]
+#FIXME:lista dos pontos do olho direito
+p_olho_dir = [160,144,158,153,33,133]
+p_olhos = p_olho_esq+p_olho_dir
+
 # enquanto a camera estiver aberta
 with mp_face_mesh.FaceMesh(min_detection_confidence=0.5,min_tracking_confidence=0.5) as facemesh:
     while cap.isOpened():
@@ -25,6 +32,8 @@ with mp_face_mesh.FaceMesh(min_detection_confidence=0.5,min_tracking_confidence=
         if not sucesso:
             print('ignorando o frame vazio da camera')
             continue
+        #FIXME: comprimento e largura
+        comprimento,largura,_ = frame.shape
         # transformando de BGR para RGB
         frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
         #FIXME: processar o frame (OpenCV - MediaPipe)
@@ -42,14 +51,21 @@ with mp_face_mesh.FaceMesh(min_detection_confidence=0.5,min_tracking_confidence=
             # 2 - face_landmarks: os landmarks detectados - pontos específicos
             # 3 - FACEMESH_CONTOURS - é uma constante que representa os contornos da face na malha facial.
             # FIXME:face_landmarks - lista de pontos (usado no projeto)
+            #NOTE:Linhas de Conexão
             mp_drawing.draw_landmarks(frame,
                                       face_landmarks,
                                       mp_face_mesh.FACEMESH_CONTOURS,
                                       landmark_drawing_spec=mp_drawing.DrawingSpec(color=(255,102,102),thickness=1,circle_radius=1),
                                       connection_drawing_spec = mp_drawing.DrawingSpec(color=(102,204,0),thickness=1,circle_radius=1)
                                       )              
-            #NOTE:Linhas de Conexão
-
+            #NOTE:Retornando  as coordenadas
+            # acessando atributo landmark - pontos
+            face = face_landmarks.landmark
+            # for      in    enumerate
+            for id_coord,coord_xyz in enumerate(face):
+                if id_coord in p_olhos:
+                    coord_cv = mp_drawing._normalized_to_pixel_coordinates(coord_xyz.x,coord_xyz.y,largura,comprimento)
+                    cv2.circle(frame,coord_cv,2,(255,0,0),-1),
         cv2.imshow('Camera',frame)
 
         if cv2.waitKey(10) & 0xFF == ord('c'):
